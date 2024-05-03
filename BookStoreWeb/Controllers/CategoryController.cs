4612b089fd3133f5add9a4d17c7c1690cbd5729e
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-05-03 12:00:09 +0200

    bootswatch theme and bootstrap icons

--- BookStoreWeb/Controllers/CategoryController.cs
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -14,7 +14,7 @@ namespace BookStoreWeb.Controllers
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
-            return View();
+            return View(objCategoryList);
         }
     }
 }
