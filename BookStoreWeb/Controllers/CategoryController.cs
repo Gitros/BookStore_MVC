9a5b46755240d75fd8c100824363c832e08249da
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-05-07 15:30:23 +0200

    Asp Validation summary

--- BookStoreWeb/Controllers/CategoryController.cs
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -24,11 +24,11 @@ namespace BookStoreWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            if(obj.Name == obj.DisplayOrder.ToString())
-            {
-                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
-            }
-            if(ModelState.IsValid)
+            //if(obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
+            //}
+            if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
