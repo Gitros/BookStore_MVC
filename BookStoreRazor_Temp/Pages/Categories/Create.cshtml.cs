e2377ac703c4102cd59c1389fe9d238f5f8e5952
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-05-08 09:33:42 +0200

    Category UI

--- BookStoreRazor_Temp/Pages/Categories/Create.cshtml.cs
+++ BookStoreRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -1,3 +1,5 @@
+using BookStoreWebRazor_Temp.Data;
+using BookStoreWebRazor_Temp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,6 +7,12 @@ namespace BookStoreWebRazor_Temp.Pages.Categories
 {
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
 
         public void OnGet()
         {
