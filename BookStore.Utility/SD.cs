702842a417d85a9a1d37d74c3aa1f072449402ac
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-06-11 13:41:08 +0200

    Order processing button logic

--- BookStore.Utility/SD.cs
+++ BookStore.Utility/SD.cs
@@ -18,6 +18,7 @@ namespace BookStore.Utility
         public const string StatusInProcess = "Processing";
         public const string StatusShipped = "Shipped";
         public const string StatusRefunded = "Refunded";
+        public const string StatusCancelled = "Cancelled";
 
         public const string PaymentStatusPending = "Pending";
         public const string PaymentStatusApproved = "Approved";
