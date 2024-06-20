9f35c49b89137086eed222818b2e681bd69eccf5
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-06-20 10:29:19 +0200

    Display company name

--- BookStoreWeb/Areas/Admin/Controllers/UserController.cs
+++ BookStoreWeb/Areas/Admin/Controllers/UserController.cs
@@ -34,6 +34,14 @@ namespace BookStoreWeb.Areas.Admin.Controllers
         {
             List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(u=>u.Company).ToList();
 
+            foreach(var user in objUserList)
+            {
+                if(user.Company == null)
+                {
+                    user.Company = new() { Name = "" };
+                }
+            }
+
             return Json(new { data = objUserList });
         }
 
